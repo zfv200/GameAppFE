9fa3b1d5a409fa65282cfd4239d1de8590896cca
Ray Blasko <mray6288@gmail.com>
2018-04-17 10:05:39 -0400

    aiming

--- src/arrow.js
+++ src/arrow.js
@@ -1,31 +1,37 @@
 class Arrow{
 
-	constructor(user_id){
+	constructor(aim){
 		this.arrow = document.createElement('div')
-		this.user_id = user_id
+		this.aim = aim
+
 	}
 
 	shoot(){
 		const area = document.getElementById('canvas')
 		// const arrow = document.createElement('div')
 		this.arrow.className = `arrow`
-		this.arrow.style.bottom = '0px'
-		this.arrow.style.left = store[0].player.style.left
+		this.arrow.style.bottom = store[0].target.style.bottom
+		this.arrow.style.left = store[0].target.style.left
 
 		area.appendChild(this.arrow)
 		this.moveArrow()
 	}
 
+	moveAim(){
+
+	}
+
 	moveArrow(){
 		let arrow = document.getElementsByClassName(`arrow`)[0]
+		let aim = this.aim
 		function curve(){
 			let left = parseInt(arrow.style.left)
 			let bottom = parseInt(arrow.style.bottom)
-			let distance = Math.abs(290 - left)
+			let distance = Math.abs(aim - left)
 			if (left < 580 && bottom >= 0) {
 				arrow.style.left = `${left + 8}px`
 			}
-			if (left < 290) {
+			if (left < aim) {
 				arrow.style.bottom = `${bottom + Math.round(distance / 30.0)}px`
 			} else {
 				if (bottom >= 0) {
--- src/eventlistener.js
+++ src/eventlistener.js
@@ -19,13 +19,21 @@ class EventListener{
 			if (e.which === 32){
 				e.preventDefault()
 				if (document.getElementsByClassName(`arrow`)[0] === undefined){
-					let arrow = new Arrow()
+					let arrow = new Arrow(player.aim)
 					arrow.shoot()
 				}
 			} else if (e.which===39){
+				e.preventDefault()
 				player.moveRight()
 			} else if (e.which===37){
+				e.preventDefault()
 				player.moveLeft()
+			} else if (e.which === 38){
+				e.preventDefault()
+				player.moveAimUp()
+			} else if (e.which === 40){
+				e.preventDefault()
+				player.moveAimDown()
 			}
 		})
 	}
