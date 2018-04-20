5e1938249944042e3d1e32b30d9362b9953091b7
github username <github email address>
2018-04-20 10:19:37 -0400

    leaderstyle

--- src/game.js
+++ src/game.js
@@ -7,8 +7,8 @@ class Game {
   static renderLogin(){
     let gameContent = document.getElementById('game-content')
     let welcomeScreen = `
-      <h3>Enter Your Username!</h3>
       <div id="welcome-content">
+      <h3>Enter Your Username!</h3>
       <form id="user-form">
       <input id="user_name_input" type="text">
       <input type="submit">
@@ -31,7 +31,7 @@ class Game {
     area.id = 'canvas'
     gameContent.innerHTML = ''
     gameContent.appendChild(area)
-    gameContent.appendChild(highScore)
+    // gameContent.appendChild(highScore)
     gameContent.appendChild(board)
     // gameContent.innerHTML += timer.render()
     // timer.increment()
---
+++ src/gameplay.js
@@ -0,0 +1,10 @@
+class Gameplay {
+  static render(){
+    return `<div id="game-play">
+      <br></br>
+      <h5>-Left and Right arrow keys to move</h5>
+      <h5>-Up and down arrow keys to aim</h5>
+      <h5>-Spacebar to shoot!</h5>
+    </div>`
+  }
+}
